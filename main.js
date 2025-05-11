0b2ddbd03e0dfa1a11003431aa990aa9b57d874a
poroshyndd <poroshyndd@gmail.com>
2025-05-11 12:59:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const http = require('http');
 
+let mainWindow;
+const SERVER_URL = 'http://localhost:3000';
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+  mainWindow.loadURL(SERVER_URL);
+}
+
+app.whenReady().then(() => {
+  const checkServer = setInterval(() => {
+    http.get(SERVER_URL, (res) => {
+      if (res.statusCode === 200) {
+        clearInterval(checkServer);
+        createWindow();
+      }
+    }).on('error', () => {
+    });
+  }, 100);
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
